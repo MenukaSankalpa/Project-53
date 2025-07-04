95c6eb9d2ad971bdcf1a68c688b914414af39400
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-07-04 14:24:56 +0530

    main.jsx file

--- QuickShow/client/src/main.jsx
+++ QuickShow/client/src/main.jsx
@@ -1,11 +1,21 @@
 
+import { ClerkProvider } from '@clerk/clerk-react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Import your Publishable Key Clerk Athuntication
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
 )
