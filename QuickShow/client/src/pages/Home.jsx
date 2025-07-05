7f339c74f376e979d8cef492e4f76892c8b77d0f
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-07-05 15:10:27 +0530

    added paths

--- QuickShow/client/src/pages/Home.jsx
+++ QuickShow/client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import FeaturedSection from '../components/FeaturedSection'
 import HeroSection from '../components/HeroSection'
+import TrailersSection from '../components/TrailersSection'
 
 const Home = () => {
   return (
     <>
-        <HeroSection/>
+        <HeroSection />
+        <FeaturedSection />
+        <TrailersSection />
     </>
   )
 }
