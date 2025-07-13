a02b768193ae1adb17fbfc11fc1cf828a2819e3b
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-07-13 16:01:28 +0530

    DateSelect.jsx file updated to add date

--- QuickShow/client/src/components/DateSelect.jsx
+++ QuickShow/client/src/components/DateSelect.jsx
@@ -16,14 +16,14 @@ const DateSelect = ({dateTime, id}) => {
                         {Object.keys(dateTime).map((date)=>(
                             <button key={date} className='flex flex-col items-center justify-center h-14 w-14 aspect-square rounded cursor-pointer'>
                                 <span>{new Date(date).getDate()}</span>
-                                <span>{new Date(date).toLocaleDateString("en-US", {month: "Short"})}</span>
+                                <span>{new Date(date).toLocaleDateString("en-US", {month: "short"})}</span>
                             </button>
                         ))}
                     </span>
                     <ChevronRightIcon width={28}/>
                 </div>
             </div>
-            <button>Book Now</button>
+            <button className='bg-primary text-white px-8 py-2 mt-6 rounded hover:bg-primary/90 transition-all cursor-pointer'>Book Now</button>
         </div>
     </div>
   )
